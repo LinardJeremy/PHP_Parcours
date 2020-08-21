2fff34940fb077b700ee056bbbf76200fdc74094
Linard Jeremy <linard.jeremy.5@gmail.com>
2020-08-21 08:18:40 +0200

    php fundamental finish

--- fonctions.php
+++ fonctions.php
@@ -73,6 +73,43 @@ function replaceInverseLetter($str){
 }
 echo "<p>",replaceInverseLetter($wordTest3),"</p>";
 
+function feedback($message, $error="info"){
+    return "<div class=$error>Error: $message.</div>";
+};
+echo feedback("Incorrect email address", "error");
+echo feedback("Incorrect email address");
+echo feedback("Incorrect email address", "test");
+
+function generateWord(){
+    $arrayLetter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    $rand1= rand(1,5);
+    $word1=[];
+    $word2=[];
+    $rand2= rand(7,15);
+    for ($i =0;$i<$rand1;$i++){
+        $randLetter = rand(0,25);
+        $word1[]=$arrayLetter[$randLetter];
+    }
+    for ($i=0;$i<$rand2;$i++){
+        $randLetter = rand(0,25);
+        $word2[]=$arrayLetter[$randLetter];
+        
+    }
+    $str = implode('',$word1);
+    $str2= implode('',$word2);
+    return  "<h1>Generate a new word</h1><p>$str</p><p>$str2</p><button>Generate</button>";
+}
+
+echo generateWord();
+echo "<p>",strtolower("STOP YELLING I CAN'T HEAR MYSELF THINKING!!"),"</p>";
+function volumeCone($ray,$height){
+    return  round($ray * $ray * 3.14 * $height * (1/3),2);  
+
+}
+echo "<p>",volumeCone(5,2),"</p>";
+echo "<p>",volumeCone(2,4),"</p>";
+echo "<p>",volumeCone(3,5),"</p>";
+
 ?>
 </body>
 </html>
